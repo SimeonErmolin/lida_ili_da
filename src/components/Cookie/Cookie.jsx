b02aa93e6752e0658fd220bb6c96a9177e4e4c98
Simeon Ermolin <ermolinsemen96@gmail.com>
2025-06-17 17:32:49 +0300

    fixes

--- src/components/Cookie/Cookie.jsx
+++ src/components/Cookie/Cookie.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cookie.scss';
 
 const Cookie = () => {
+  const [isShow, setShow] = useState(true);
+
   return (
-    <div className="cookie-wrapper">
-      <img src="public/assets/icons/funnyCookie.svg" alt="" />
+    <>
+      {isShow && (
+        <div className="cookie-wrapper">
+          <img src="public/assets/icons/funnyCookie.svg" alt="" />
 
-      <div>
-        <p>Мы используем cookie-файлы</p>
-        <p>Для повышения удобства работы с сайтом</p>
-      </div>
+          <div>
+            <p>Мы используем cookie-файлы</p>
+            <p>Для повышения удобства работы с сайтом</p>
+          </div>
 
-      <button>Хорошо</button>
-    </div>
+          <button onClick={() => setShow(false)}>Хорошо</button>
+        </div>
+      )}
+    </>
   );
 };
 
